c712c67a333b9671496508192e0e56df644e1e69
da crands <davecrands@gmail.com>
2018-07-05 22:22:13 -0400

    Working on typsetting for about

--- src/components/About.js
+++ src/components/About.js
@@ -1,20 +1,43 @@
 import React, { Component } from 'react';
 
 export default class Hello extends Component {
+
+    constructor() {
+        super();
+
+        this.state = {
+            skills: ['React', 'Redux', 'Express', 'Sass', 'Webpack']
+        }
+    }
+
     render() {
         return(
             <div>
                 <div className="container">
-                    <div className="center">
+                    <div className="center about">
                         <h1>About</h1>
                         <hr className="hr"/>
-                        <p>I really enjoy building websites using
-                            react, webpack, and express.
+                        <p class="lead">
+                            I really enjoy building fast, responsive, and intuitive Node apps.
                         </p>
-                        <p>
-                            My apps are responsive, fast, and
-                            intuitive.
+                        <div className="three">
+                            <p></p>
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos ipsum molestiae optio delectus nemo autem nisi dicta, odit eius ea!</p>
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos ipsum molestiae optio delectus nemo autem nisi dicta, odit eius ea!</p>
+                        </div>
+
+                        
+                        <p>Most of my projects use the following tech:                            
                         </p>
+                        <ul>
+                            {
+                                this.state.skills.map(skill => {
+                                    return (
+                                        <li>{skill}</li>
+                                    )
+                                })
+                            }
+                        </ul>
                     </div>                
                 </div>            
             </div>
