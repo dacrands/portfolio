32efdbc439479b3525e880dd522effd9937c6f20
da crands <davecrands@gmail.com>
2018-10-13 10:34:01 -0400

    Added formspree, form button styling

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -9,21 +9,22 @@ export default class Contact extends Component {
           <header className="header">
             <h1 className={"primary"}>Contact</h1>
           </header>
-          <form className="contact" action="">
+          <form className="contact" action="https://formspree.io/davecrands@gmail.com" method="POST">
             <div className="contact__item">
                 <label htmlFor="name">Name</label>
                 <input type="text"/>                      
             </div>
             <div className="contact__item">
               <label htmlFor="email">Email</label>            
-              <input type="email"/>          
+              <input type="email" name="_replyto"/>          
             </div>
             <div className="contact__item">
               <label htmlFor="message">Message</label>         
               <br/>
               <textarea name="message" id="message" cols="30" rows="10" placeholder={"Hey Dave, \n\nThis site rocks! Thanks for making it. \n\nRegards,\nTextarea Placeholder"}></textarea>
             </div>                
-            <input type="submit"/>              
+            <button className="button" type="submit">Send <span>&rsaquo;</span> </button>
+            {/* <input className="button" type="submit" value="Send &rsaquo;"/>               */}
           </form>
         </div>
       </div>
